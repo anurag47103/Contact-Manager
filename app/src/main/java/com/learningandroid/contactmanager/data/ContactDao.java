e82230de8b672e5c5df8b502bbeff9ebf96e30a8
anurag47103 <anuragsharma47103@gmail.com>
2021-09-07 15:01:24 +0530

    added ContactDao

---
+++ app/src/main/java/com/learningandroid/contactmanager/data/ContactDao.java
@@ -0,0 +1,23 @@
+package com.learningandroid.contactmanager.data;
+
+import androidx.core.view.WindowInsetsAnimationCompat;
+import androidx.lifecycle.LiveData;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.learningandroid.contactmanager.model.Contact;
+
+import java.util.List;
+
+public interface ContactDao  {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Contact contact);
+
+    @Query("delete from contact_table")
+    void deleteALL();
+
+    @Query("select * from contact_table order by name asc")
+    LiveData<List<Contact>> getAllContact();
+}

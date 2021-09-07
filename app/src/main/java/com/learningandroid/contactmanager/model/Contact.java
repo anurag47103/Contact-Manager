4fe98f7e7956e54356529f6aaa87d39e8c191da5
anurag47103 <anuragsharma47103@gmail.com>
2021-09-07 14:56:19 +0530

    added model class Contact

---
+++ app/src/main/java/com/learningandroid/contactmanager/model/Contact.java
@@ -0,0 +1,45 @@
+package com.learningandroid.contactmanager.model;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "contact_table")
+public class Contact {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String name;
+
+
+    private String occupation;
+
+    public Contact(String name, String occupation) {
+        this.name = name;
+        this.occupation = occupation;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+}
